8825a14993b2a6c4183164d60d81826c1ebed801
Dino-lllakil0Nil <dinar.shagaliev2002@gmail.com>
2020-07-13 00:28:30 +0300

    add ready Student Repository

---
+++ src/ru/itis/repositories/StudentsRepository.java
@@ -0,0 +1,9 @@
+package ru.itis.repositories;
+
+import ru.itis.models.Student;
+
+import java.util.List;
+
+public interface StudentsRepository extends CrudRepository<Student> {
+    List<Student> findAllByAge(int age);
+}
